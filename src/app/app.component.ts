b743868f5a59e9b5eb8eb6c710b8538a4148f624
Muhammed Ashik <ashik@collab.net>
2017-05-08 21:31:36 -0700

    Adding Angular2 Client.
    Change-Id: I8f92b60e88b0b010e0f4fade307c9d96d6bddc76

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graphql-client-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'graphql-client works!';
+}
