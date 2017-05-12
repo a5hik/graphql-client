95a5f149e10aceb2efe811f83632c588a49d507d
Muhammed Ashik <ashik@collab.net>
2017-05-11 20:33:46 -0700

    Adding graphql client stack.
    Change-Id: Idc65844fd0a01ef6ab3fd5b112acd6c65615805d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import * as faker from 'faker';
+
+import gql from 'graphql-tag';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'graphql-client-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'graphql-client works!';
+export class AppComponent implements OnInit, OnDestroy {
+
+  users: any;
+  sub: any;
+
+  constructor(private apollo: Apollo) {
+  }
+
+  ngOnInit() {
+    this.users = this.apollo.watchQuery<any>({
+      query: gql`
+        query {
+          users {
+            firstName
+            lastName
+            email
+          }
+        }
+      `
+    }).map(result => result.data.users);
+
+    this.apollo.subscribe({
+      query: gql`
+        subscription {
+          user(filter: { mutation_in: CREATED }) {
+            node {
+              id
+              email
+            }
+          }
+        }
+      `
+    }).subscribe((data) => {
+      this.users.updateQuery((prev) => {
+        const users = prev.users || [];
+
+        return {
+          ...prev,
+          users: [...users, data.user.node]
+        };
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    this.sub = undefined;
+  }
+
+  createRandomUser() {
+    this.apollo.mutate({
+      mutation: gql`
+        mutation ($firstName: String!, $lastName: String!, $email: String!) {
+          createUser(firstName: $firstName, lastName: $lastName, email: $email) {
+            id
+            firstName
+            lastName
+            email
+          }
+        }
+      `,
+      variables: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email()
+      }
+    }).toPromise();
+  }
+
 }
